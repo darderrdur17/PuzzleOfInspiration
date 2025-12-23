d0c433540ce0aedc295b117ad64bf00fc99c8cc4
darderrdur17 <e0960472@u.nus.edu>
2025-12-23 10:33:42 +0700

    fix: Exclude puzzle-quest from Next.js build
    - Added puzzle-quest to tsconfig.json exclude array
    - Updated next.config.js to ignore puzzle-quest in webpack
    - Prevents Next.js from trying to compile separate Vite project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Exclude puzzle-quest from Next.js build (it's a separate Vite project)
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  webpack: (config, { isServer }) => {
+    // Exclude puzzle-quest from webpack compilation
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/node_modules/**', '**/puzzle-quest/**'],
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
